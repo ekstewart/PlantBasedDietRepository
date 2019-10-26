cc2fe808b485e26a4fa876e14cae3f81b73a4207
Pedro Suazo <pedrosuazo10@gmail.com>
2019-10-25 18:50:32 -0700

    Recipe class and Ingredient class created.

---
+++ src/com/company/Recipe.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Recipe {
+
+    private String title;
+    private String instructions;
+    private ArrayList<Ingredient> ingredientList;
+}
